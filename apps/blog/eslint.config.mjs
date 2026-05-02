e48f28be0a66c03b72d9b0e4c8be57890211778f
jongmin lee <ljm920604@gmail.com>
2026-05-02 20:26:27 +0900

    Chore: 공통 ESLint 설정 패키지 추가
    - packages/eslint-config 패키지를 추가해 Next 앱 공통 ESLint 설정을 분리
    - 블로그 앱의 eslint.config.mjs가 공통 설정을 가져오도록 변경
    - @atelier/eslint-config 워크스페이스 의존성 추가
    - ESLint 설정 패키지와 tsconfig 패키지 설명 노트 보강
    - 저장소 문서의 현재 공유 패키지 상태 갱신

--- apps/blog/eslint.config.mjs
+++ apps/blog/eslint.config.mjs
@@ -1,18 +1,3 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import nextAppConfig from "@atelier/eslint-config/next-app";
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
-
-export default eslintConfig;
+export default nextAppConfig;
